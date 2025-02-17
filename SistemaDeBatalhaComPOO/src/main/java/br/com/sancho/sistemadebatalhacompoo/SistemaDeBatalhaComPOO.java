7b061e8f8751c4c605c4fad97186acd6caa953e6
Francisco Sancho <frsancho1985@gmail.com>
2025-02-16 22:33:19 -0300

    Instanciando objetos

--- SistemaDeBatalhaComPOO/src/main/java/br/com/sancho/sistemadebatalhacompoo/SistemaDeBatalhaComPOO.java
+++ SistemaDeBatalhaComPOO/src/main/java/br/com/sancho/sistemadebatalhacompoo/SistemaDeBatalhaComPOO.java
@@ -6,16 +6,16 @@ public class SistemaDeBatalhaComPOO {
 
     public static void main(String[] args) {
 
-        Arma espadaLonga = new Arma("Espada Longa", 5);
-        Arma cajadoArcano = new Arma("Cajado Arcano", 8);
+        Arma emeraldSword = new Arma("Emerald Sword", 5);
+        Arma CajadoDoMagoBranco = new Arma("Cajado do Mago Branco", 8);
         Arma arcoElfico = new Arma("Arco Élfico", 7);
 
         Guerreiro guerreiro = new Guerreiro("Conan", "Guerreiro", 100, 5, 25, 8);
         Mago mago = new Mago("Gandalf", "Mago", 100, 5, 30, 3);
-        Arqueiro arqueiro = new Arqueiro("Legolas", "Arqueiro", 100, 5, 16, 10);
+        Arqueiro arqueiro = new Arqueiro("Robbin Hood", "Arqueiro", 100, 5, 16, 10);
 
-        guerreiro.equiparArma(espadaLonga);
-        mago.equiparArma(cajadoArcano);
+        guerreiro.equiparArma(emeraldSword);
+        mago.equiparArma(CajadoDoMagoBranco);
         arqueiro.equiparArma(arcoElfico);
 
         SistemaBatalha batalha = new SistemaBatalha(mago, arqueiro);
